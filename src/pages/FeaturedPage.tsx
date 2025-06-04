77bff13b50168af28b973fbfca7bb149206ca75d
yuchanandrew <anrh8554@colorado.edu>
2025-06-04 00:55:45 -0600

    Animations for each page added.

--- src/pages/FeaturedPage.tsx
+++ src/pages/FeaturedPage.tsx
@@ -1,9 +1,16 @@
 import Card from "../components/Card";
+import { motion } from "framer-motion";
 
 const FeaturedPage = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1.2, ease: "easeInOut", damping: 25 }}
+        className="flex flex-col items-center justify-center"
+      >
         <div className="flex flex-col p-6 mt-2">
           <h2 className="text-2xl font-semibold text-gray-200">
             Featured Items
@@ -14,7 +21,7 @@ const FeaturedPage = () => {
           description="A placeholder t-shirt. Lorem Ipsum."
           image="/camo_placeholder.jpeg"
         />
-      </div>
+      </motion.div>
     </>
   );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,33 +2,42 @@ import Top from "../components/Top";
 import "../App.css";
 import Card from "../components/Card";
 
+import { motion } from "framer-motion";
+
 function HomePage() {
   return (
     <>
-      <div className="relative z-40">
-        <Top />
-      </div>
-      <main className="flex-1 min-h-screen">
-        <section className="flex flex-col items-center justify-center">
-          <div className="flex bg-white w-8/11 rounded-xl gap-10 items-center justify-center min-h-[350px]">
-            <div className="flex flex-col gap-6">
-              <h2 className="text-5xl font-bold -translate-x-20">
-                Lorem Ipsum
-              </h2>
-              <h2 className="text-5xl text-green-600 font-bold translate-x-5">
-                Lorem Ipsum
-              </h2>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1.2, ease: "easeInOut", damping: 25 }}
+      >
+        <div className="relative z-40">
+          <Top />
+        </div>
+        <main className="flex-1 min-h-screen mt-8">
+          <section className="flex flex-col items-center justify-center">
+            <div className="flex bg-white w-8/11 rounded-xl gap-10 items-center justify-center min-h-[350px]">
+              <div className="flex flex-col gap-6">
+                <h2 className="text-5xl font-bold -translate-x-20">
+                  Lorem Ipsum
+                </h2>
+                <h2 className="text-5xl text-green-600 font-bold translate-x-5">
+                  Lorem Ipsum
+                </h2>
+              </div>
+              <div className="flex translate-x-20">
+                <Card
+                  title="Camo Tee"
+                  description="A placeholder t-shirt. Lorem Ipsum."
+                  image="/camo_placeholder.jpeg"
+                />
+              </div>
             </div>
-            <div className="flex translate-x-20">
-              <Card
-                title="Camo Tee"
-                description="A placeholder t-shirt. Lorem Ipsum."
-                image="/camo_placeholder.jpeg"
-              />
-            </div>
-          </div>
-        </section>
-      </main>
+          </section>
+        </main>
+      </motion.div>
     </>
   );
 }
